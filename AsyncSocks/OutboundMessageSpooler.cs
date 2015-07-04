811f0e5fc71ebc8638addcc688f2bb4ce496ca44
Adrian Perez Heredia <aperezhrd@gmail.com>
2015-07-04 19:34:30 +0200

    Refactored tests for OutboundMessageSpooler

--- AsyncSocks/OutboundMessageSpooler.cs
+++ AsyncSocks/OutboundMessageSpooler.cs
@@ -3,13 +3,17 @@ using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 
 namespace AsyncSocks
 {
-    public class OutboundMessageSpooler
+    public class OutboundMessageSpooler : IRunnable
     {
         private ITcpClient tcpClient;
         private BlockingCollection<byte[]> queue;
+        private AutoResetEvent startEvent = new AutoResetEvent(false);
+        private bool running;
+        private bool shouldStop;
 
         public OutboundMessageSpooler(ITcpClient tcpClient, BlockingCollection<byte[]> queue)
         {
@@ -21,6 +25,32 @@ namespace AsyncSocks
         {
             byte[] message = queue.Take();
             tcpClient.Write(message, 0, message.Length);
+        }
+
+        public void Run()
+        {
+            running = true;
+            startEvent.Set();
+            while (!shouldStop)
+            {
+                Spool();
+            }
+            running = false;
+        }
+
+        public void Stop()
+        {
+            shouldStop = true;
+        }
+
+        public bool IsRunning
+        {
+            get { return running; }
+        }
+
+        public bool WaitStarted()
+        {
+            return startEvent.WaitOne();
         }
     }
 }
--- Tests/AsyncSocks_Tests/Tests/OutboundMessageSpoolerTest.cs
+++ Tests/AsyncSocks_Tests/Tests/OutboundMessageSpoolerTest.cs
@@ -4,28 +4,63 @@ using System.Collections.Concurrent;
 using AsyncSocks;
 using Moq;
 using System.Text;
+using System.Threading;
 
 namespace AsyncSocks_Tests
 {
     [TestClass]
     public class OutboundMessageSpoolerTest
     {
+        private OutboundMessageSpooler spooler;
+        private Mock<ITcpClient> tcpClientMock;
+        private BlockingCollection<byte[]> queue;
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            queue = new BlockingCollection<byte[]>(new ConcurrentQueue<byte[]>());
+            tcpClientMock = new Mock<ITcpClient>();
+            spooler = new OutboundMessageSpooler(tcpClientMock.Object, queue);
+        }
+        
         [TestMethod]
         public void SpoolShouldWriteMessageToTcpClient()
         {
-            BlockingCollection<byte[]> queue = new BlockingCollection<byte[]>(new ConcurrentQueue<byte[]>());
-            Mock<ITcpClient> tcpClientMock = new Mock<ITcpClient>();
-
             string messageString = "This is a test message";
             byte[] messageBytes = Encoding.ASCII.GetBytes(messageString);
 
             tcpClientMock.Setup(x => x.Write(messageBytes, 0, messageBytes.Length)).Returns(messageBytes.Length).Verifiable();
-            OutboundMessageSpooler spooler = new OutboundMessageSpooler(tcpClientMock.Object, queue);
 
             queue.Add(messageBytes);
 
             spooler.Spool();
+        }
+
+        [TestMethod]
+        public void ShouldImplementIRunnable()
+        {
+            Assert.IsTrue(spooler is IRunnable);
+        }
+
+        [TestMethod]
+        public void RunShouldCallSpool()
+        {
+            AutoResetEvent spoolerCalled = new AutoResetEvent(false);
+
+            queue.Add(new byte[5]{0,1,2,3,4});
+            tcpClientMock.Setup(x => x.Write(It.IsAny<byte[]>(), It.IsAny<int>(), It.IsAny<int>())).Callback(() => spoolerCalled.Set()).Returns(5).Verifiable(); // If Write is called, it means Spool also has been called.
+
+            ThreadRunner runner = new ThreadRunner(spooler);
+            runner.Start();
+            
+            spoolerCalled.WaitOne(2000);
+            
+            runner.Stop();
+
+            tcpClientMock.Verify();
 
         }
+
+        
     }
 }
