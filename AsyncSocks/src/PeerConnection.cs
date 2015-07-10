ed98ecfa692e043b41c20c9b55cfd48ba1792003
Adrian Perez Heredia <aperezhrd@gmail.com>
2015-07-10 11:04:11 +0200

    Added PeerConnection and tests

---
+++ AsyncSocks/src/PeerConnection.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsyncSocks
+{
+    public class PeerConnection
+    {
+        private IInboundMessageSpooler inboundSpooler;
+        private IOutboundMessageSpooler outboundSpooler;
+
+        public PeerConnection(IInboundMessageSpooler inboundSpooler, IOutboundMessageSpooler outboundSpooler)
+        {
+            this.inboundSpooler = inboundSpooler;
+            this.outboundSpooler = outboundSpooler;
+        }
+
+        public void SendMessage(byte[] messageBytes)
+        {
+            outboundSpooler.Enqueue(messageBytes);
+        }
+    }
+}
---
+++ Tests/AsyncSocks_Tests/Tests/PeerConnectionTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AsyncSocks;
+using Moq;
+using System.Text;
+
+namespace AsyncSocks_Tests.Tests
+{
+    [TestClass]
+    public class PeerConnectionTest
+    {
+        [TestMethod]
+        public void SendMessageShouldTellOutboundMessageSpoolerToEnqueueTheMessage()
+        {
+            var inboundSpoolerMock = new Mock<IInboundMessageSpooler>();
+            var outboundSpoolerMock = new Mock<IOutboundMessageSpooler>();
+
+            var inboundSpooler = inboundSpoolerMock.Object;
+            var outboundSpooler = outboundSpoolerMock.Object;
+
+            PeerConnection connection = new PeerConnection(inboundSpooler, outboundSpooler);
+
+            byte[] messageBytes = Encoding.ASCII.GetBytes("This is a test message");
+
+            outboundSpoolerMock.Setup(x => x.Enqueue(messageBytes)).Verifiable();
+            
+            connection.SendMessage(messageBytes);
+
+            outboundSpoolerMock.Verify();
+        }
+    }
+}
