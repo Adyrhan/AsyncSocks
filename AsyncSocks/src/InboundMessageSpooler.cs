996b984e06aec69cdbfd88344c7a2af4cf6def11
Adrian Perez Heredia <aperezhrd@gmail.com>
2015-07-10 08:27:31 +0200

    Moved asyncsocks sources from asynsocks root project to src folder

---
+++ AsyncSocks/src/InboundMessageSpooler.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace AsyncSocks
+{
+    public class InboundMessageSpooler : ThreadRunner
+    {
+        public static InboundMessageSpooler Create(ITcpClient tcpClient)
+        {
+            //NetworkMessageReader reader
+            //InboundMessageSpoolerRunnable runnable = new InboundMessageSpoolerRunnable(reader, queue);
+            //return new InboundMessageSpooler();
+
+            throw new NotImplementedException();
+        }
+
+        public InboundMessageSpooler(IInboundMessageSpoolerRunnable runnable) : base(runnable) { }
+
+    }
+}
