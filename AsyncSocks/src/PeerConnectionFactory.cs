ef7949629d8ae0c7ab00987986ba31e702d19c06
Adrian Perez Heredia <aperezhrd@gmail.com>
2015-07-12 14:41:10 +0200

    Implemented PeerConnectionFactory

---
+++ AsyncSocks/src/PeerConnectionFactory.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncSocks
+{
+    class PeerConnectionFactory : IPeerConnectionFactory
+    {
+        public IPeerConnection Create(IInboundMessageSpooler inboundSpooler, IOutboundMessageSpooler outboundSpooler, ITcpClient tcpClient)
+        {
+            return new PeerConnection(InboundMessageSpooler.Create(tcpClient), OutboundMessageSpooler.Create(tcpClient), tcpClient);
+        }
+    }
+}
