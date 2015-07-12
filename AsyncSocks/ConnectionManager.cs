76c119efb80ab83b96408f657af9c14fa2fe1b05
Adrian Perez Heredia <aperezhrd@gmail.com>
2015-07-12 09:49:09 +0200

    ConnectionManager creates spoolers and PeerConnection objects and adds them to its dictionary.

---
+++ AsyncSocks/ConnectionManager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+
+namespace AsyncSocks
+{
+    public class ConnectionManager
+    {
+        private Dictionary<IPEndPoint, PeerConnection> dict;
+
+        public void Add(ITcpClient tcpClient)
+        {
+            var inboundSpooler = InboundMessageSpooler.Create(tcpClient);
+            var outboundSpooler = OutboundMessageSpooler.Create(tcpClient);
+            var connection = new PeerConnection(inboundSpooler, outboundSpooler, tcpClient);
+
+            dict[(IPEndPoint) tcpClient.Client.RemoteEndPoint] = connection;
+        }
+
+        public ConnectionManager(Dictionary<IPEndPoint, PeerConnection> dict)
+        {
+            this.dict = dict;
+        }
+    }
+}
---
+++ Tests/AsyncSocks_Tests/Tests/ConnectionManagerTests.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AsyncSocks;
+using Moq;
+using System.Collections.Generic;
+using System.Net;
+
+namespace AsyncSocks_Tests.Tests
+{
+    [TestClass]
+    public class ConnectionManagerTests
+    {
+        private ConnectionManager connManager;
+        private Mock<Dictionary<IPEndPoint, PeerConnection>> dictMock;
+        private Dictionary<IPEndPoint, PeerConnection> dict;
+
+        [TestInitialize]
+        public void BeforeEach()
+        {
+            dictMock = new Mock<Dictionary<IPEndPoint, PeerConnection>>();
+            dict = new Dictionary<IPEndPoint, PeerConnection>();
+            connManager = new ConnectionManager(dict);
+        }
+        
+        [TestMethod]
+        public void AddCreatesANewPeerConnectionAndAddsItToItsList()
+        {
+            var tcpClientMock = new Mock<ITcpClient>();
+            var socketMock = new Mock<ISocket>();
+            
+            var endPoint = new IPEndPoint(IPAddress.Parse("80.80.80.80"), 80);
+            tcpClientMock.Setup(x => x.Client).Returns(socketMock.Object).Verifiable();
+            socketMock.Setup(x => x.RemoteEndPoint).Returns(endPoint).Verifiable();
+
+            connManager.Add(tcpClientMock.Object);
+
+            tcpClientMock.Verify();
+            socketMock.Verify();
+            Assert.AreEqual(endPoint, dict[endPoint].RemoteEndPoint); 
+        }
+    }
+}
