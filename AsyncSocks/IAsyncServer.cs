42d845937052f5d053d93c07e4732b9aa3c691d6
Adrian Perez Heredia <aperezhrd@gmail.com>
2015-07-18 12:22:53 +0200

    Created interface for AsyncServer

---
+++ AsyncSocks/IAsyncServer.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AsyncSocks
+{
+    public interface IAsyncServer
+    {
+        event NewClientMessageDelegate OnNewMessageReceived;
+        void Start();
+        void Stop();
+    }
+}
