b03d335e23782a0112a96dae9b65506b477d88ac
Adrian Perez Heredia <aperezhrd@gmail.com>
2015-07-28 10:12:49 +0200

    AsyncClient.Create is now static method

--- AsyncSocks/src/AsyncClient.cs
+++ AsyncSocks/src/AsyncClient.cs
@@ -82,7 +82,7 @@ namespace AsyncSocks
             get { return tcpClient; }
         }
 
-        public AsyncClient Create(IPEndPoint remoteIpAddress)
+        public static AsyncClient Create(IPEndPoint remoteIpAddress)
         {
             BaseTcpClient client = new BaseTcpClient(new TcpClient(remoteIpAddress));
             return (AsyncClient)new PeerConnectionFactory().Create(client);
